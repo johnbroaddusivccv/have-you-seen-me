222d54fa927758a09a64845c2cf8b3124b2dfb5a
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-06-10 01:35:08 -0500

    scrapped old connection, connected to database and server work/routing in the same folder/file Locking repo down making calls to the database..

---
+++ sql-server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+
+const SELECT_ALL_PERSONS_QUERY = "SELECT * FROM namusmissing";
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "missingperson"
+});
+
+connection.connect(err => {
+  if (err) {
+    return err;
+  }
+});
+console.log(connection);
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("go to /persons to see missing people");
+});
+
+app.get("/persons", (req, res) => {
+  connection.query(SELECT_ALL_PERSONS_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+app.listen(4000, () => console.log(`sql server listening on port 4000`));
