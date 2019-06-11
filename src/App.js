b168425e97895623ff0c12ba440583ea7e5a8a5b
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-06-11 18:28:10 -0500

    added data to the dom so i can see everything I am thinking about working with

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo, { ReactComponent } from "./logo.svg";
 import "./App.css";
+import { link } from "fs";
 
 class App extends Component {
   state = {
@@ -18,14 +19,22 @@ class App extends Component {
       .catch(err => console.error(err));
   };
   // This is redndering the picture link to every Missing person in the Namus Database
-  renderPerson = ({ person_id, image }) => (
+  renderPerson = ({ person_id, image, link, ...stuff }) => (
     <div key={person_id}>
+      {stuff["First Name"]}
+      {stuff["Last Name"]}
+      {stuff["Race / Ethnicity"]}
+      {stuff["City Of Last Contact"]}
+
+      {stuff["geo_point_2d"]}
+
       <img src={image} alt="missingperson" />
     </div>
   );
 
   render() {
     const { persons } = this.state;
+    console.log(persons);
     return <div className="App">{persons.map(this.renderPerson)}</div>;
   }
 }
